dd4b54a9e9420ca182e4ee18712f79c8185727dc
KIM Taegyoon <steloflute@gmail.com>
2016-05-26 16:44:55 +0900

    qualified expression

--- euler17.adb
+++ euler17.adb
@@ -1,42 +1,40 @@
 with Ada.Text_IO;
 
 procedure Euler17 is
-   function Length(A : String) return Natural is (A'Length);
-   
    Word_Lengths : array(Natural range <>) of Natural :=
      (
-      Length(""),
-      Length("one"),
-      Length("two"),
-      Length("three"),
-      Length("four"),
-      Length("five"),
-      Length("six"),
-      Length("seven"),
-      Length("eight"),
-      Length("nine"),
-      Length("ten"),
-      Length("eleven"),
-      Length("twelve"),
-      Length("thirteen"),
-      Length("fourteen"),
-      Length("fifteen"),
-      Length("sixteen"),
-      Length("seventeen"),
-      Length("eighteen"),
-      Length("nineteen"));
+      String'("")'Length,
+      String'("one")'Length,
+      String'("two")'Length,
+      String'("three")'Length,
+      String'("four")'Length,
+      String'("five")'Length,
+      String'("six")'Length,
+      String'("seven")'Length,
+      String'("eight")'Length,
+      String'("nine")'Length,
+      String'("ten")'Length,
+      String'("eleven")'Length,
+      String'("twelve")'Length,
+      String'("thirteen")'Length,
+      String'("fourteen")'Length,
+      String'("fifteen")'Length,
+      String'("sixteen")'Length,
+      String'("seventeen")'Length,
+      String'("eighteen")'Length,
+      String'("nineteen")'Length);
    
    Word_Ty_Lengths : array(Natural range <>) of Natural :=
-     (Length(""),
-      Length(""),
-      Length("twenty"),
-      Length("thirty"),
-      Length("forty"),
-      Length("fifty"),
-      Length("sixty"),
-      Length("seventy"),
-      Length("eighty"),
-      Length("ninety"));
+     (String'("")'Length,
+      String'("")'Length,
+      String'("twenty")'Length,
+      String'("thirty")'Length,
+      String'("forty")'Length,
+      String'("fifty")'Length,
+      String'("sixty")'Length,
+      String'("seventy")'Length,
+      String'("eighty")'Length,
+      String'("ninety")'Length);
    
    function Word_Length(N : Natural) return Natural is
       Sum : Natural;
@@ -46,11 +44,11 @@ procedure Euler17 is
       elsif N <= 99 then
 	 return Word_Ty_Lengths(N / 10) + Word_Lengths(N mod 10);
       elsif N = 1000 then
-	 return Length("onethousand");
+	 return String'("onethousand")'Length;
       else
-	 Sum := Word_Lengths(N / 100) + Length("hundred");
+	 Sum := Word_Lengths(N / 100) + String'("hundred")'Length;
 	 if N mod 100 > 0 then
-	    Sum := Sum + Length("and") + Word_Length(N mod 100);
+	    Sum := Sum + String'("and")'Length + Word_Length(N mod 100);
 	 end if;
 	 return Sum;
       end if;
