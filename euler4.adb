9b12d59aefbb1e0da2d9b7308de463c8fc579294
KIM Taegyoon <steloflute@gmail.com>
2016-06-07 01:58:12 +0900

    math

--- euler4.adb
+++ euler4.adb
@@ -1,26 +1,52 @@
-with ada.text_io, ada.strings.fixed;
-use ada.text_io;
+with Ada.Text_IO, Ada.Strings.Fixed;
+use Ada.Text_IO;
 
-procedure euler4 is
-	function is_palindrome(s : string) return boolean is
-	begin
-		for i in s'first .. s'last / 2 loop
-			if s(i) /= s(s'last - i + 1) then
-				return false;
-			end if;
-		end loop;
-		return true;
-	end;
-	max_p : integer := 0;
-	p : integer;
+procedure Euler4 is
+   function Is_Palindrome(S : String) return Boolean is
+   begin
+      for I in S'First .. S'Last / 2 loop
+	 if S(I) /= S(S'Last - I + 1) then
+	    return False;
+	 end if;
+      end loop;
+      return True;
+   end;
+   
+   function Is_Palindrome(N : Integer) return Boolean is
+      Temp : Integer := N;
+      Reversed : Integer := 0;
+   begin
+      while Temp > 0 loop
+	 Reversed := Reversed * 10 + Temp mod 10;
+	 Temp := Temp / 10;
+      end loop;
+      return N = Reversed;
+   end;
+   
+   Max_P : Integer;
+   P : Integer;
 begin
-	for i in 100 .. 999 loop
-		for j in i .. 999 loop
-			p := i * j;
-			if is_palindrome(ada.strings.fixed.trim(integer'image(p), ada.strings.left)) and then p > max_p then
-				max_p := p;
-			end if;
-		end loop;
-	end loop;
-	put_line(integer'image(max_p));
-end euler4;
+   -- string
+   Max_P := 0;
+   for I in 100 .. 999 loop
+      for J in I .. 999 loop
+	 P := I * J;
+	 if Is_Palindrome(Ada.Strings.Fixed.Trim(Integer'Image(P), Ada.Strings.Left)) and then P > Max_P then
+	    Max_P := P;
+	 end if;
+      end loop;
+   end loop;
+   Put_Line(Integer'Image(Max_P));
+   
+   -- math
+   Max_P := 0;
+   for I in 100 .. 999 loop
+      for J in I .. 999 loop
+	 P := I * J;
+	 if Is_Palindrome(P) and then P > Max_P then
+	    Max_P := P;
+	 end if;
+      end loop;
+   end loop;
+   Put_Line(Integer'Image(Max_P));
+end;
